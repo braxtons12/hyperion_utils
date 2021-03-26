dde4cd9571e2910d54cc3dbea57de8088a1737ff
braxtons12 <braxtonsalyer@gmail.com>
2021-03-25 22:09:17 -0500

    Another change to ignore

--- include/HyperionUtils/Ignore.h
+++ include/HyperionUtils/Ignore.h
@@ -10,6 +10,6 @@ namespace hyperion::utils {
 	/// @param args - The things to ignore
 	template<typename... Args>
 	inline constexpr auto ignore(Args&&... args) noexcept -> void {
-		std::ignore = std::make_tuple<Args...>(args...);
+		std::ignore = std::make_tuple<Args...>(std::forward<Args...>(args...));
 	}
 } // namespace hyperion::utils
