41deda5a9eb3b862c95415f4b5d6c5455dd7f611
braxtons12 <6ypqal4t>
2021-10-28 20:12:35 -0500

    For Format.h:
    Continue directory hierarchy change.
    
    wrap fmtlib includes with more warning ignorers.
    
    Document Format.h

---
+++ include/Hyperion/Format.h
@@ -0,0 +1,58 @@
+/// @file Format.h
+/// @author Braxton Salyer <braxtonsalyer@gmail.com>
+/// @brief singular include point for fmtlib
+/// @version 0.1
+/// @date 2021-10-15
+///
+/// MIT License
+/// @copyright Copyright (c) 2021 Braxton Salyer <braxtonsalyer@gmail.com>
+///
+/// Permission is hereby granted, free of charge, to any person obtaining a copy
+/// of this software and associated documentation files (the "Software"), to deal
+/// in the Software without restriction, including without limitation the rights
+/// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+/// copies of the Software, and to permit persons to whom the Software is
+/// furnished to do so, subject to the following conditions:
+///
+/// The above copyright notice and this permission notice shall be included in all
+/// copies or substantial portions of the Software.
+///
+/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+/// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+/// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+/// SOFTWARE.
+
+#pragma once
+
+#include <Hyperion/HyperionDef.h>
+
+IGNORE_PADDING_START
+IGNORE_SIGNED_BITFIELD_START
+IGNORE_UNKNOWN_DOC_COMMAND_START
+IGNORE_WEAK_VTABLES_START
+IGNORE_CONSTRUCTOR_SHADOW_FIELDS_START
+IGNORE_COMMA_MISUSE_START
+IGNORE_RESERVED_IDENTIFIERS_START
+IGNORE_MISSING_NORETURN_START
+IGNORE_CONSTANT_CONDITIONAL_EXPRESSIONS_START
+IGNORE_DEPRECATED_DECLARATIONS_START
+
+#include <fmt/chrono.h>
+#include <fmt/color.h>
+#include <fmt/compile.h>
+#include <fmt/format.h>
+#include <fmt/os.h>
+
+IGNORE_DEPRECATED_DECLARATIONS_STOP
+IGNORE_CONSTANT_CONDITIONAL_EXPRESSIONS_STOP
+IGNORE_MISSING_NORETURN_STOP
+IGNORE_RESERVED_IDENTIFIERS_STOP
+IGNORE_COMMA_MISUSE_STOP
+IGNORE_CONSTRUCTOR_SHADOW_FIELDS_STOP
+IGNORE_WEAK_VTABLES_STOP
+IGNORE_UNKNOWN_DOC_COMMAND_STOP
+IGNORE_SIGNED_BITFIELD_STOP
+IGNORE_PADDING_STOP
