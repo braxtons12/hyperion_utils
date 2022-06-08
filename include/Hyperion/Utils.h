da2827707f81dcbccf29e893ccc2841973606e13
braxtons12 <braxtonsalyer@gmail.com>
2022-06-07 21:42:40 -0500

    remove using decls from Utils.h

--- include/Hyperion/Utils.h
+++ include/Hyperion/Utils.h
@@ -1,10 +1,33 @@
-/// @brief Hyperion is a Hyperion Engine library consisting of various basic
-/// functionalities, types, and utilities.
+/// @file Utils.h
+/// @author Braxton Salyer <braxtonsalyer@gmail.com>
+/// @brief Hyperion Utils is a Hyperion Engine library constisting of various core vocabulary types,
+/// such as `Option<T>`, `Result<T, E>` and `Error<StatusCodeDomain>`; owning synchronization types,
+/// such as `ReadWriteLock<T>`; alternatives to certain standard library types, such as
+/// `UniquePtr<T, Deleter>`; custom File type in `fs::File`; Meta-programming utilites in
+/// `hyperion::mpl`; and a robust, compile-time configured logging system; among other utilites.
+/// @version 0.1
+/// @date 2022-06-07
 ///
-/// Hyperion contains Hyperion's logging, error handling, and meta-programming
-/// facilities, as well as various datastructures and replacements or wrappers
-/// for `std` types such as `Variant` to replace `std::variant` or `Span` to
-/// wrap `gsl::span`
+/// MIT License
+/// @copyright Copyright (c) 2022 Braxton Salyer <braxtonsalyer@gmail.com>
+///
+/// Permission is hereby granted, free of charge, to any person obtaining a copy
+/// of this software and associated documentation files (the "Software"), to deal
+/// in the Software without restriction, including without limitation the rights
+/// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+/// copies of the Software, and to permit persons to whom the Software is
+/// furnished to do so, subject to the following conditions:
+///
+/// The above copyright notice and this permission notice shall be included in all
+/// copies or substantial portions of the Software.
+///
+/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+/// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+/// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+/// SOFTWARE.
 #pragma once
 
 #include <Hyperion/BasicTypes.h>
@@ -24,45 +47,3 @@
 #include <Hyperion/Synchronization.h>
 #include <Hyperion/TypeTraits.h>
 #include <Hyperion/test/OptionAndResultTests.h>
-
-using hyperion::Err;  // NOLINT(misc-unused-using-decls,
-					  // google-global-names-in-headers)
-using hyperion::None; // NOLINT(misc-unused-using-decls,
-					  // google-global-names-in-headers)
-using hyperion::Ok;	  // NOLINT(misc-unused-using-decls,
-					  // google-global-names-in-headers)
-using hyperion::Some; // NOLINT(misc-unused-using-decls,
-					  // google-global-names-in-headers)
-
-using hyperion::u16;   // NOLINT(misc-unused-using-decls, google-global-names-in-headers)
-using hyperion::u32;   // NOLINT(misc-unused-using-decls, google-global-names-in-headers)
-using hyperion::u64;   // NOLINT(misc-unused-using-decls, google-global-names-in-headers)
-using hyperion::u8;	   // NOLINT(misc-unused-using-decls, google-global-names-in-headers)
-using hyperion::umax;  // NOLINT(misc-unused-using-decls, google-global-names-in-headers)
-using hyperion::usize; // NOLINT(misc-unused-using-decls, google-global-names-in-headers)
-
-using hyperion::i16;  // NOLINT(misc-unused-using-decls, google-global-names-in-headers)
-using hyperion::i32;  // NOLINT(misc-unused-using-decls, google-global-names-in-headers)
-using hyperion::i64;  // NOLINT(misc-unused-using-decls, google-global-names-in-headers)
-using hyperion::i8;	  // NOLINT(misc-unused-using-decls, google-global-names-in-headers)
-using hyperion::imax; // NOLINT(misc-unused-using-decls, google-global-names-in-headers)
-
-using hyperion::f32;  // NOLINT(misc-unused-using-decls, google-global-names-in-headers)
-using hyperion::f64;  // NOLINT(misc-unused-using-decls, google-global-names-in-headers)
-
-using hyperion::operator""_u8;	  // NOLINT(misc-unused-using-decls, google-global-names-in-headers)
-using hyperion::operator""_u16;	  // NOLINT(misc-unused-using-decls, google-global-names-in-headers)
-using hyperion::operator""_u32;	  // NOLINT(misc-unused-using-decls, google-global-names-in-headers)
-using hyperion::operator""_u64;	  // NOLINT(misc-unused-using-decls, google-global-names-in-headers)
-using hyperion::operator""_usize; // NOLINT(misc-unused-using-decls, google-global-names-in-headers)
-using hyperion::operator""_umax;  // NOLINT(misc-unused-using-decls, google-global-names-in-headers)
-
-using hyperion::operator""_i8;	 // NOLINT(misc-unused-using-decls, google-global-names-in-headers)
-using hyperion::operator""_i16;	 // NOLINT(misc-unused-using-decls, google-global-names-in-headers)
-using hyperion::operator""_i32;	 // NOLINT(misc-unused-using-decls, google-global-names-in-headers)
-using hyperion::operator""_i64;	 // NOLINT(misc-unused-using-decls, google-global-names-in-headers)
-using hyperion::operator""_imax; // NOLINT(misc-unused-using-decls, google-global-names-in-headers)
-
-using hyperion::operator""_f32;	 // NOLINT(misc-unused-using-decls, google-global-names-in-headers)
-using hyperion::operator""_f64;	 // NOLINT(misc-unused-using-decls, google-global-names-in-headers)
-using hyperion::operator""_fmax; // NOLINT(misc-unused-using-decls, google-global-names-in-headers)
