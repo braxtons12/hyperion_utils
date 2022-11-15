d35a43b571557617883bb986addbf595085870de
Braxton Salyer <braxtonsalyer@gmail.com>
2022-11-14 19:23:56 -0600

    Cleanup package file

--- export/packages/h/hyperion-utils/xmake.lua
+++ export/packages/h/hyperion-utils/xmake.lua
@@ -54,37 +54,3 @@ package("hyperion-utils")
         end
         import("package.tools.xmake").install(package, configs)
     end)
---add_deps("doctest", "fmt", "gsl", { system = false })
---add_deps("boost", {
---    alias = "boost",
---    system = false,
---    external = true,
---    configs = {
---        container = true,
---        exception = true,
---        python = false,
---        stacktrace = true,
---        languages = "cxx20"
---    },
---})
---add_deps("vcpkg::tracy", {
---    alias = "tracy",
---    system = false,
---    configs = {
---        languages = "cxx20"
---    },
---})
-
---on_load(function (package)
---    package:set("installdir", path.join(os.scriptdir(), package:plat(), package:arch(), package:mode()))
---end)
-
---on_fetch(function (package)
---    local result = {}
---    local libfiledir = (package:config("shared") and package:is_plat("windows", "mingw")) and "bin" or "lib"
---    result.links = "hyperion-utils"
---    result.linkdirs = package:installdir("lib")
---    result.includedirs = package:installdir("include")
---    result.libfiles = path.join(package:installdir(libfiledir), "libhyperion-utils.a")
---    return result
---end)
